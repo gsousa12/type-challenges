b192b514011864cd4c74796670b73cc0193344a7
gsousa12 <wgsousa12@gmail.com>
2025-12-22 07:44:08 -0300

    resolution

---
+++ resolutions/00002-medium-return-type/index.ts
@@ -0,0 +1,28 @@
+import { logger } from "../utils/logger";
+
+/*
+https://github.com/type-challenges/type-challenges/blob/main/questions/00002-medium-return-type/README.md
+*/
+
+type MyReturnType<T extends (...args: never[]) => unknown> = T extends (
+  ...args: never[]
+) => infer R
+  ? R
+  : never;
+
+function __test() {
+  const fn = (v: boolean) => {
+    if (v) return 1;
+    else return 2;
+  };
+
+  type a = MyReturnType<typeof fn>; // should be "1 | 2"
+
+  const testA: a = 1;
+  const testB: a = 2;
+
+  logger("00002-medium-return-type");
+  console.log(testA, testB);
+}
+
+__test();
